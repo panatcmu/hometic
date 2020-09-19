6a349449a6ab1eeea76f331ddad2db9f7cb4bff4
panatcmu <69357981+panatcmu@users.noreply.github.com>
2020-09-19 14:24:02 +0700

    heroku address

--- server.go
+++ server.go
@@ -6,6 +6,7 @@ import (
    "github.com/gorilla/mux"
    "log"
    "net/http"
+   "os"
 )
 // git init, git add server.go go.mod, git commit -m "[Nong] init project"
 func main(){
@@ -15,9 +16,12 @@ func main(){
    r := mux.NewRouter()
    r.HandleFunc("/pair-device", PairDeviceHandler).Methods(http.MethodPost)
 
+   addr := fmt.Sprintf("0.0.0.0:%s", os.Getenv("PORT"))
+
 
    server := http.Server{
-      Addr:    "127.0.0.1:2009",
+	//   Addr:    "127.0.0.1:2009",
+	  Addr:    addr,
       Handler: r,
    }
    log.Println("starting...")
